5efb3b812d4c2af4bb735601ce71b41ea54c821a
Himanshu Neema <himanshun.iitkgp@gmail.com>
2017-01-03 22:31:17 -0500

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
+# To change parameters edit settings.cfg 
+
 ./wikirace '{
     "start":"https://en.wikipedia.org/wiki/Malaria",
     "end":"https://en.wikipedia.org/wiki/Geophysics"
 }' 
-
-# By default it runs BFS algorithm , to run
-# ID-DFS pass one more parameter in the end: 
-
-#./wikirace '{
-#    "start":"https://en.wikipedia.org/wiki/Malaria",
-#    "end":"https://en.wikipedia.org/wiki/Geophysics"
-#}' IDDFS
